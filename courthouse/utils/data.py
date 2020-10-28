a09d30312d5cfdd44fa03bfd2fce3c22a72b88ac
heidariarash <arashheidari94@outlook.com>
2020-10-28 18:49:17 +0330

    numpy categorical manipulator complete

--- courthouse/utils/data.py
+++ courthouse/utils/data.py
@@ -43,7 +43,7 @@ class PandasManipulator():
 
 class NumpyNumericalManipulator():
     """
-    Use this class to manipulate a Pandas dataframe and get new data.
+    Use this class to manipulate a Numpy array and get new data.
     """
     def __init__(self):
         self.__column_change = 0
@@ -52,7 +52,7 @@ class NumpyNumericalManipulator():
 
     def config(self, **kwargs):
         """
-        Use this method to configure the PandasManipulator settings.
+        Use this method to configure the NumpyNumericalManipulator settings.
         """
         for key, value in kwargs.items():
             if key == "column_to_change":
@@ -74,8 +74,41 @@ class NumpyNumericalManipulator():
 
         else:
             for i, item in enumerate(new_arr[:, self.__column_change]):
-                print(i, item)
                 if item == self.__old_value:
                     new_arr[i, self.__column_change] = self.__new_value
+
+        return new_arr
+
+
+class NumpyCategoricalManipulator():
+    """
+    Use this class to manipulate a Numpy Array and get new data.
+    """
+    def __init__(self):
+        self.__old_columns = 0
+        self.__new_column = 0
+
+    def config(self, **kwargs):
+        """
+        Use this method to configure the NumpyCategoricalManipulator settings.
+        """
+        for key, value in kwargs.items():
+            if key == "old_columns":
+                self.__old_columns = value
+
+            elif key == "new_column":
+                self.__new_column = value
+
+    def apply(self, array):
+        """
+        Use this method to apply the manipulation on a dataframe
+        """
+        new_arr = array.copy()
+        
+        for i, item in enumerate(new_arr[:, self.__old_columns]):
+            if (item == 1).any():
+                print(new_arr[i, self.__new_column])
+                new_arr[i, self.__new_column] = 1
+                new_arr[i, self.__old_columns] = 0
 
         return new_arr
