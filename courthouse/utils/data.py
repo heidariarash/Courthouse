f6780f69b973d7768b6050f670c7731850e3eb5e
heidariarash <arashheidari94@outlook.com>
2020-10-28 18:34:50 +0330

    Numpy Numeric Manipulator added

--- courthouse/utils/data.py
+++ courthouse/utils/data.py
@@ -7,8 +7,8 @@ class PandasManipulator():
     """
     def __init__(self):
         self.__column_change = ""
-        self.__column_value = ""
-        self.__column_old = None
+        self.__new_value = ""
+        self.__old_value = None
 
     def config(self, **kwargs):
         """
@@ -19,10 +19,10 @@ class PandasManipulator():
                 self.__column_change = value
 
             elif key == "new_value":
-                self.__column_value = value
+                self.__new_value = value
 
             elif key == "old_value":
-                self.__column_old = value
+                self.__old_value = value
 
     def apply(self, dataframe):
         """
@@ -32,10 +32,50 @@ class PandasManipulator():
             raise Exception(f"dataframe does not have a column named {self.__column_change}.")
 
         new_df = dataframe.copy()
-        if self.__column_old == None:
-            new_df[self.__column_change] = self.__column_value
+        if self.__old_value == None:
+            new_df[self.__column_change] = self.__new_value
 
         else:
-            new_df[self.__column_change] = new_df[self.__column_change].replace(self.__column_old, self.__column_value)
+            new_df[self.__column_change] = new_df[self.__column_change].replace(self.__old_value, self.__new_value)
 
         return new_df
+
+
+class NumpyNumericalManipulator():
+    """
+    Use this class to manipulate a Pandas dataframe and get new data.
+    """
+    def __init__(self):
+        self.__column_change = 0
+        self.__new_value = ""
+        self.__old_value = None
+
+    def config(self, **kwargs):
+        """
+        Use this method to configure the PandasManipulator settings.
+        """
+        for key, value in kwargs.items():
+            if key == "column_to_change":
+                self.__column_change = value
+
+            elif key == "new_value":
+                self.__new_value = value
+
+            elif key == "old_value":
+                self.__old_value = value
+
+    def apply(self, array):
+        """
+        Use this method to apply the manipulation on a dataframe
+        """
+        new_arr = array.copy()
+        if self.__old_value == None:
+            new_arr[:,self.__column_change] = self.__new_value
+
+        else:
+            for i, item in enumerate(new_arr[:, self.__column_change]):
+                print(i, item)
+                if item == self.__old_value:
+                    new_arr[i, self.__column_change] = self.__new_value
+
+        return new_arr
