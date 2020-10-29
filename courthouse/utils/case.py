0d3fb1667574cdcb5566caa9dc21a70d95693509
heidariarash <arashheidari94@outlook.com>
2020-10-29 17:31:33 +0330

    Categorical Case added

---
+++ courthouse/utils/case.py
@@ -0,0 +1,19 @@
+from typing import Union
+
+class CategoricalCase():
+    """
+    Use this class to specify a categorical case.
+    """
+    def __init__(self, column: int, name: str) -> None:
+        self.__column = column
+        self.__name = column
+
+    def get(self, key: str) -> Union(str, int):
+        """
+        Thie is a getter method for CategoricalCase class.
+        """
+        if key == "column":
+            return self.__column
+
+        elif key == "name":
+            return self.__name
