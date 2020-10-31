40cef3691d15032b73de22b8cff38e07a935f915
heidariarash <arashheidari94@outlook.com>
2020-10-31 18:17:12 +0330

    verdict case for binary_sigmoid complete

--- courthouse/tensorflow/judge.py
+++ courthouse/tensorflow/judge.py
@@ -13,6 +13,7 @@ class CategoricalJudge:
         self.__new_case = None
         self.__org_out = None
         self.__new_out = None
+        self.__output_type = None
 
     def case(self, data: np.ndarray, change_from: CategoricalCase , change_towards: CategoricalCase) -> None:
         """
@@ -31,6 +32,7 @@ class CategoricalJudge:
         """
         org_predict = model.predict(self.__org_data)
         new_predict = model.predict(self.__org_data)
+        self.__output_type = output_type
         if output_type == "categorical":
             self.__org_out = []
             self.__new_out = []
@@ -41,7 +43,7 @@ class CategoricalJudge:
             for output in new_predict:
                 self.__new_out.append(np.argmax(output))
 
-        if output_type == "binary":
+        if output_type == "binary_sigmoid":
             self.__org_out = []
             self.__new_out = []
 
@@ -50,6 +52,40 @@ class CategoricalJudge:
 
             for output in new_predict:
                 self.__new_out.append(1 if output>=0.5 else 0)
+
+        if output_type == "binary_tanh":
+            self.__org_out = []
+            self.__new_out = []
+
+            for output in org_predict:
+                self.__org_out.append(1 if output>=0 else 0)
+
+            for output in new_predict:
+                self.__new_out.append(1 if output>=0 else 0)
+
+        else:
+            self.__output_type = None
+            raise Exception(f'{output_type} output_type is not defined.')
             
     def verdict(self) -> str:
-        pass
+        """
+        Use this method to print the report of the fairness of the model.
+        """
+        #checking if the model is actually judged
+        if self.__output_type is None:
+            print('No model has been judged yet.')
+
+        print(f"There are {self.__org_data.shape[0]} \"{self.__old_case.get('name')}\" to be found.\n")
+        print("When the model was applied to the original dataset, these results where obtained:")
+        if self.__output_type == "binary_sigmoid":
+            ones = sum(filter(lambda x: x==1, self.__org_out))
+            print(f"\t{ones} time(s) the model predict 1. This is the case for {ones/len(self.__org_out)*100}% of the data.")
+            print(f"\t{len(self.__org_out) - ones} time(s) the model predict 0. This is the case for {(1 - ones/len(self.__org_out))* 100}% of the data.\n")
+
+            print(f"Then dataset was changed in a way that all the {self.__old_case.get('name')} were changed to {self.__new_case.get('name')}.\n")
+            print("These results were obtained after applying the model on the new data.")
+
+            ones = sum(filter(lambda x: x==1, self.__new_out))
+            print(f"\t{ones} time(s) the model predict 1. This is the case for {ones/len(self.__new_out) * 100}% of the data.")
+            print(f"\t{len(self.__new_out) - ones} time(s) the model predict 0. This is the case for {(1 - ones/len(self.__new_out)) * 100}% of the data.")
+
