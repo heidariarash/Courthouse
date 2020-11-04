976c7cd4bd2f1877ddae5d36c5e175abda89883b
heidariarash <arashheidari94@outlook.com>
2020-11-04 18:30:01 +0330

    numerical case added

--- courthouse/utils/case.py
+++ courthouse/utils/case.py
@@ -10,7 +10,26 @@ class CategoricalCase:
 
     def get(self, key: str) -> Union[str, int]:
         """
-        Thie is a getter method for CategoricalCase class.
+        This is a getter method for CategoricalCase class.
+        """
+        if key == "column":
+            return self.__column
+
+        elif key == "name":
+            return self.__name
+
+
+class NumericalCase:
+    """
+    Use this class to specify a numerical case.
+    """
+    def __init__(self, column: int, name: str) -> None:
+        self.__column = column
+        self.__name = name
+
+    def get(self, key: str) -> Union[str, int]:
+        """
+        This is a getter method for NumericalCase.
         """
         if key == "column":
             return self.__column
