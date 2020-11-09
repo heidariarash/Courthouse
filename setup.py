17cb16f4960323b5f73d55c5333fe674796156f3
heidariarash <arashheidari94@outlook.com>
2020-11-09 18:56:13 +0330

    updating gitignore and setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
   author = 'Arash Heidari',                   # Type in your name
   author_email = 'arashheidari94@outlook.com',      # Type in your E-Mail
   url = 'https://github.com/heidariarash/Courthouse',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/heidariarash/Courthouse/archive/v_01.tar.gz',    # I explain this later on
   keywords = ['Machine Learning', 'Fairness', 'PyTorch', 'TensorFlow', 'Deep Learning'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'numpy',
